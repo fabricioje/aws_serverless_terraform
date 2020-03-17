c91da9904e6f2b2d0a58b6eda40ed18af366583f
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2020-03-17 11:01:51 -0300

    - Implementado TF Template

--- terraform/infra/users/iam-role-login.tf
+++ terraform/infra/users/iam-role-login.tf
@@ -2,21 +2,7 @@ resource "aws_iam_role" "login_iam_role" {
 
     name = "${var.environment}-login-iam-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = templatefile("${path.module}/templates/lambda-base-policy.tpl", {})
   
 }
 
