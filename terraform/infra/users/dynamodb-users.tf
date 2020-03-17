43698a37b59fcd33fb2d429fe4b6fed5d525effd
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2020-03-17 10:28:08 -0300

    - Criando Global Secondary Indexes e exportando ele

--- terraform/infra/users/dynamodb-users.tf
+++ terraform/infra/users/dynamodb-users.tf
@@ -7,11 +7,31 @@ resource "aws_dynamodb_table" "users" {
   }
   write_capacity = "${var.write_capacity}"
   read_capacity  = "${var.read_capacity}"
-}
 
+  #Atributo para a criação do Global Secunday Index
+  attribute{
+    name = "email"
+    type = "S"
+  }
+  global_secondary_index{
+    name            = "${var.environment}-email-gsi" #Nome do hash_key do secundary index
+    projection_type = "ALL" # Quais dados serão projetados na tabela
+    hash_key        = "email" #É o nome do atribuito que foi definido, nome do hash_key do index
+    write_capacity  = "${var.write_capacity}"
+    read_capacity   = "${var.read_capacity}"
+  }
+}
 resource "aws_ssm_parameter" "dynamodb_users_table" {
   name  = "${var.environment}-dynamodb-users-table"
   type  = "String"
   value = "${aws_dynamodb_table.users.name}"
 }
+
+#Exporta o index criado para o SSM
+resource "aws_ssm_parameter" "email-gsi" {
+  name  = "${var.environment}-email-gsi"
+  type  = "String"
+  value = "${var.environment}-email-gsi"
+}
+
 
