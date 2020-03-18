ef2fe1a939b5d5a4c19aada4222ac4d0d009be4a
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2020-03-18 15:36:35 -0300

    - Criado IAM Role para a Lambda list-bookings

---
+++ terraform/infra/bookings/iam-role-list-bookings.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_role" "list_bookings_iam_role" {
+
+    name = "${var.environment}-list-bookings-iam-role"
+
+  assume_role_policy = templatefile("${path.module}/templates/lambda-base-policy.tpl", {})
+  
+}
+
+//Paramentro que será exportado
+resource "aws_ssm_parameter" "list_bookings_iam_role" {
+  name = "${var.environment}-list-bookings-iam-role"
+  type = "String"
+  value = "${aws_iam_role.list_bookings_iam_role.arn}"
+}
+
