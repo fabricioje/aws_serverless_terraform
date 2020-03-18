cd0d8cea0cef7545186795d3c13f7a3acd10d780
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2020-03-18 09:41:36 -0300

    Criando tabela de Bookings no DynamoDB

---
+++ terraform/infra/bookings/dynamodb-bookings.tf
@@ -0,0 +1,18 @@
+resource "aws_dynamodb_table" "bookings" {
+  name = "${var.environment}-bookings"
+  hash_key = "id"
+  attribute{
+      name = "id"
+      type = "S"
+  }
+  write_capacity = "${var.write_capacity}"
+  read_capacity = "${var.read_capacity}"
+}
+
+//Cria o recurso no SSM
+resource "aws_ssm_parameter" "dynamodb_bookings_table" {
+  name = "${var.environment}-dynamodb-bookings-table"
+  type = "String"
+  value = "${aws_dynamodb_table.bookings.name}"
+}
+
