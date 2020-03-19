5a39d8b7de5f9ccd651fe97560c54a285ca91eab
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2020-03-19 10:53:15 -0300

    - Formatado a mensagem de confirmação de agendamento

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -2,10 +2,14 @@
 
 const AWS = require ('aws-sdk')
 const converter = AWS.DynamoDB.Converter
-
+const moment = require('moment')
+moment.locale('pt-br')
 module.exports.listen = async event => {
   for(const record of event.Records){
-    console.log('JSON parseado', converter.unmarshall(record.dynamodb.NewImage))
+    if (record.eventName === 'INSERT') {
+      const reserva = converter.unmarshall(record.dynamodb.NewImage)
+      console.log(`Reserva efetuada: o usuário ${reserva.user.name} (${reserva.user.email}) agendou um horário em: ${moment(reserva.date).format('LLLL')}`)
+    }
   }
   return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
